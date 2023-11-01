a38c4a49c20c70716e3894dc135e5d75740e613c
Yahia Mekki <mekki.yahia0411@gmail.com>
2023-11-01 17:56:26 +0100

    Adding template directive

--- resources/views/app/master.blade.php
+++ resources/views/app/master.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>TP WEB</title>
+    <title>@yield('title')</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -176,13 +176,7 @@
         <!-- Main content -->
         <section class="content">
             <div class="container-fluid">
-                <!-- Info boxes -->
-
-                <!-- /.row -->
-
-                <!-- Main row -->
-
-                <!-- /.row -->
+               @yield('content')
             </div><!--/. container-fluid -->
         </section>
         <!-- /.content -->
