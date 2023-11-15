f0ecbde18b01690683c66465ebb0aab2d4608889
Mohamedbourek <87719740+Mohamedbourek@users.noreply.github.com>
2023-11-15 21:11:50 +0100

    Some new routes in web

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -8,11 +9,33 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\ConsommateurController;
+use App\Models\User;
+
+Route::group(['prefix' => 'backoffice/Consommateurs'], function () {
+    
+
+    // Route to show the inscription form
+    Route::get('inscription', [UserController::class, 'showInscriptionForm'])->name('consommateur.inscription.form');
+
+    // Route to handle the inscription form submission
+    Route::post('inscription', [UserController::class, 'handleInscription'])->name('consommateur.inscription.handle');
+
+});
+
+
+
+
+Route::get('backoffice/artisans', function () {
+    return view('backoffice.Artisans.index');
+});
+
