076596a157406b7d3abc95ef5e4538a4ded24357
Mohamedbourek <87719740+Mohamedbourek@users.noreply.github.com>
2023-12-15 22:10:41 +0100

    Add Authentification for All Users

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Consommateur;
+use App\Models\Artisan;
+use App\Models\Livreur;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Session; 
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view('front_office/identification');
+    }
+    public function loginPost(Request $request)
+    {
+        $credetials = [
+            'Email' => $request->Email,
+            'mdps' => $request->mdps,
+        ];
+        $user = User::where('Email' , '=' ,$request->Email)->first();
+        if($user && $request->mdps == $user->mdps)
+        {
+            switch ($user->Role) {
+                
+                case 'Artisan':
+                    $artisan = Artisan::where('Email' , '=' ,$request->Email)->first();
+                    Session::put('artisan',$artisan);
+                    echo "Artisan DashBoard";
+                    break;
+                case 'Consommateur':
+                    $consumer = Consommateur::where('Email' , '=' ,$request->Email)->first();
+                    Session::put('consumer',$consumer);
+                    return Redirect::route('home')->with('success', 'Login successful');
+                    break;
+                case 'Livreur':
+                    $livreur = Livreur::where('Email' , '=' ,$request->Email)->first();
+                    Session::put('livreur',$livreur);
+                    echo "Livreur DashBoard";
+                    break;
+            
+        }
+    }
+        else{
+          echo 'Email or password incorrect';
+        }
+    }
+    public function logout()
+    {
+        Auth::logout();
+        Session::flush();
+        return redirect()->route('home');
+    }
+}
