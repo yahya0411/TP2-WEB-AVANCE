5e5ee8e6d7a0dcaa19413a86ed2c015d3311302c
Mohamedbourek <87719740+Mohamedbourek@users.noreply.github.com>
2023-12-16 16:53:00 +0100

    Add Models to include DataBase

---
+++ app/Models/Artisan.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Artisan extends Model
+{
+    protected $fillable = [
+        'nom_artisan', 
+        'téléphone',
+        'Email', 
+    ];
+    protected $primaryKey = 'id_artisan';
+    public function produits()
+{
+    return $this->hasMany(Produit::class, 'id_artisan');
+}
+}
